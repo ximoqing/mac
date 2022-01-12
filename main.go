31497af9128702b350f8140f6fd764ac8a1c095e
ximoqing <3328323781@qq.com>
2022-01-13 01:16:22 +0800

    添加mac生成

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"fmt"
+)
 
-func main() {
-	fmt.Println("hell")
+func MacInit() (mac string) {
+	buf := make([]byte, 6)
+	_, err := rand.Read(buf)
+	if err != nil {
+		return
+	}
+	buf[0] |= 2
+	mac = fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5])
+	return mac
 }
